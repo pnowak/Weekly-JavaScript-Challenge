6c05ac86f4f77f125d4e521d7a11dc0dace362b2
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-09-18 22:56:53 +0200

    debunce, populateStorage and lastRead

--- 3/js/script.js
+++ 3/js/script.js
@@ -1,92 +1,42 @@
-var keys = {
-    handleKeypress: function (e) {
-        e = e || window.event; // IE
-        
-        if (e.which) {
-            count.keys.push(e.which);
-            keys.fire('textwrite', keys);
-        }
-    },
-    handleKeydown: function (e) {
-        e = e || window.event; // IE
-        
-        if (e.which === 8 || e.which === 46) {
-            count.keys.pop(e.which);
-            keys.fire('textdelete', keys);
-        }
-    },
-    valueLength : function (e) {
-        e = e || window.event; // IE
-
-        if (e.ctrlKey && e.which === 86) {
-            console.log('ctrl+v!');
-            count.keys.push(e.which);
-            keys.fire('copypast', keys);
-        }
-    }
-};
-
 var count = {
     c : get('count'),
-    send : get('sendButton'),
-    keys : [],
-    length : function () {
-        var txt = this.c.textContent,
-            parts = txt.split(' '),
-            len = this.keys.length;
+    last : function () {
+        var txt = count.c.textContent,
+            parts = txt.split(':');
 
-        parts[0] = parseInt(parts[0], 10) - len;
-        this.c.textContent = parts[0] + ' ' + parts[1] + ' ' + parts[2];
-    },
-    countDown : function () {
-        var txt = this.c.textContent,
-            parts = txt.split(' ');
-
-        parts[0] = parseInt(parts[0], 10) - 1;
-        this.c.textContent = parts[0] + ' ' + parts[1] + ' ' + parts[2];
-    },
-    countUp : function () {
-        var txt = this.c.textContent,
-            parts = txt.split(' ');
-
-        parts[0] = parseInt(parts[0], 10) + 1;
-        this.c.textContent = parts[0] + ' ' + parts[1] + ' ' + parts[2];
-    },
-    reset : function () {
-        var txt = this.c.textContent,
-            parts = txt.split(' ');
-
-        parts[0] = 35;
-        this.c.textContent = parts[0] + ' ' + parts[1] + ' ' + parts[2];
+        parts[1] = lastRead();
+        count.c.textContent = parts[0] + ': ' + parts[1];
     }
 };
 
-var ransom = {
-    message : [],
-    getValue : function (e) {
-        var input = get('inputMessage'),
-            value = input.value,
-            array = value.split('');
+function lastRead() {
+    localStorage.lastRead = (new Date()).toUTCString();
+    return new Date(Date.parse(localStorage.lastRead));
+}
 
-        ransom.fire('value', ransom);
-        ransom.fire('reset', ransom);
+function populateStorage(id) {
+    localStorage.setItem(id, get(id).value);
+}
 
-        input.value = '';
-
-        if (typeof e.stopPropagation === "function") {
-            e.stopPropagation();
-        }
-        e.cancelBubble = true;
-
-        if (typeof e.preventDefault === "function") {
-            e.preventDefault();
-        }
-        e.returnValue = false;
+var debounce = function(func, delay) {
+    var inDebounce = undefined;
+    return function() {
+        var context = this,
+            args = arguments;
+        clearTimeout(inDebounce);
+        return inDebounce = setTimeout(function() {
+            return func.apply(context, args);
+        }, delay);
     }
-};
+}
 
-window.onkeypress = keys.handleKeypress;
-window.onkeydown = keys.handleKeydown;
+var form = get('form');
+
+form.addEventListener('change', debounce(function() {
+    return populateStorage('wystawiona');
+}, 3000));
+
+form.addEventListener('change', count.last);
 
 function get(id) {
     return document.getElementById(id);

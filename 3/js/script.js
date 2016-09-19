3564db43ea35fc6e0b9e974a4d17cd3b2be5391c
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-09-19 23:13:54 +0200

    localStorage worked

--- 3/js/script.js
+++ 3/js/script.js
@@ -9,15 +9,6 @@ var count = {
     }
 };
 
-function lastRead() {
-    localStorage.lastRead = (new Date()).toUTCString();
-    return new Date(Date.parse(localStorage.lastRead));
-}
-
-function populateStorage(id) {
-    localStorage.setItem(id, get(id).value);
-}
-
 var debounce = function(func, delay) {
     var inDebounce = undefined;
     return function() {
@@ -33,11 +24,57 @@ var debounce = function(func, delay) {
 var form = get('form');
 
 form.addEventListener('change', debounce(function() {
-    return populateStorage('wystawiona');
+    return savethestuffLocal();
 }, 3000));
 
 form.addEventListener('change', count.last);
 
+function lastRead() {
+    localStorage.lastRead = (new Date()).toUTCString();
+    return new Date(Date.parse(localStorage.lastRead));
+}
+
+function savethestuffLocal() {
+    var data = {
+        firstname: '',
+        lastname: '',
+        kulica: '',
+        kmiasto: '',
+        kkod: '',
+        name: '',
+        fulica: '',
+        fmiasto: '',
+        fkod: '',
+        nip: '',
+        money: '',
+        wystawiona: '',
+        doreczona: '',
+        spozniona: '',
+        oplacona: ''
+    };
+    localStorage.data = JSON.stringify(data);
+    var data = JSON.parse(localStorage.data),
+        prop;
+    for (prop in data) {
+        if (data.hasOwnProperty(prop)) {
+            localStorage.setItem(prop, get(prop).value);
+        }
+    }
+    getthestuffLocal();
+}
+
+function getthestuffLocal() {
+    var data = JSON.parse(localStorage.data),
+        prop;
+    for (prop in data) {
+        if (data.hasOwnProperty(prop)) {
+            get(prop).value = localStorage.getItem(prop);
+        }
+    }
+}
+
 function get(id) {
     return document.getElementById(id);
 }
+
+window.onload  = getthestuffLocal();

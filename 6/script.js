9f6f4f7548e8142003010c7f5ffd98f8de863492
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-09-21 19:33:20 +0200

    closed tags

--- 6/script.js
+++ 6/script.js
@@ -2,7 +2,9 @@
 
 class Tag {
 
-    constructor() {}
+    constructor() {
+        this.data = {}
+    }
 
     get(id) {
         return document.getElementById(id);
@@ -10,25 +12,49 @@ class Tag {
 
     createTag(value) {
         const result = this.get('result');
-        const frag = document.createDocumentFragment();
         const tag = document.createElement('li');
         const content = document.createTextNode(value);
-        const close = document.createElement('a').addEventListener( 'click', function() {
-            console.log('usuniemy cię');
-        } );
 
-        tag.appendChild(content);
-        frag.appendChild(tag);
-        result.insertBefore(frag, result.firstChild);
+        return new Promise(
+            function(resolve, reject) {
+                tag.appendChild(content);
+                resolve(result.insertBefore(tag, result.firstChild))
+            }
+        ).then(
+            function(val) {
+                val.insertAdjacentHTML('beforeend', '<button class="close">x</button>');
+                localStorage.setItem(val.textContent, val.textContent);
+        })
+        .catch(
+            function(reason) {
+                console.log('Handle rejected promise ('+ reason +').');
+        });
+    }
+
+    closeTag(e) {
+        const src = e.target;
+        const parent = src.parentNode;
+
+        if (src.nodeName.toLowerCase() === "button") {
+            localStorage.removeItem(parent.textContent, parent.textContent);
+            parent.parentNode.removeChild(parent);
+        }
     }
 
     getValue(e) {
         const input = this.get('input');
         const value = input.value;
 
+        if (value.trim() === '') {
+            throw new Error('Empty input');
+        }
+
         if (value.includes(',')) {
             let a = value.split(',');
             for (let i = 0; i < a.length; i += 1) {
+                if (a[i].trim() === '') {
+                    continue;
+                }
                 this.createTag(a[i]);
             }
         } else {
@@ -47,10 +73,24 @@ class Tag {
         }
         e.returnValue = false;
     }
+
+    getthestuffLocal() {
+        var data = JSON.parse(localStorage.data),
+            prop;
+        for (prop in data) {
+            if (data.hasOwnProperty(prop)) {
+                prop.value = localStorage.getItem(prop.value);
+            }
+        }
+    }
 };
 
 const tag = new Tag();
 
 const send = tag.get('send');
+const result = tag.get('result');
 
 send.addEventListener('click', (e) => { tag.getValue(e) }, false);
+result.addEventListener( 'click', (e) => { tag.closeTag(e) }, false);
+
+window.onload  = tag.getthestuffLocal();

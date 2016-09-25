76ffe2a4f3988eb15b015b36f8e8e2603977e0cc
pnowak <piotr.nowak.25.02.1978@gmail.com>
2016-09-25 21:16:10 +0200

    add localStorage

--- 6/script.js
+++ 6/script.js
@@ -2,7 +2,9 @@
 
 class Tag {
 
-    constructor() {}
+    constructor() {
+        this.tags = [];
+    }
 
     get(id) {
         return document.getElementById(id);
@@ -12,6 +14,7 @@ class Tag {
         const result = this.get('result');
         const tag = document.createElement('li');
         const content = document.createTextNode(value);
+        const tags = this.tags;
 
         return new Promise(
             function(resolve, reject) {
@@ -24,6 +27,7 @@ class Tag {
                 return val;
         }).then(
             function(val) {
+                tags.push(val.textContent);
                 localStorage.setItem(val.textContent, val.textContent);
         })
         .catch(
@@ -37,6 +41,10 @@ class Tag {
         const parent = src.parentNode;
 
         if (src.nodeName.toLowerCase() === "button") {
+            let index = this.tags.indexOf(parent.textContent);
+            if (index != -1) {
+                this.tags.splice(index, 1);
+            }
             localStorage.removeItem(parent.textContent);
             parent.parentNode.removeChild(parent); 
         }
@@ -76,9 +84,10 @@ class Tag {
     }
 
     getLocal() {
-        let tags = JSON.parse(localStorage.tags);
+        let tags = localStorage;
         for (let tag in tags) {
-            tag.textContent = localStorage.getItem(tag);
+            tag = tag.substr(0, tag.lastIndexOf('x'));
+            this.createTag(tag);
         }
     }
 };
